e05a3542644d4a091a81312b659b0e5e2e3afe81
labkumar.singh <labkumar.singh@172-11-6-137.lightspeed.jcvlfl.sbcglobal.net>
2017-02-23 11:52:35 +0530

    Miscellaneous update

---
+++ Example/CustomTabBarBadge/FirstViewController.swift
@@ -0,0 +1,47 @@
+//
+//  FirstViewController.swift
+//  CustomTabBarBadge
+//
+//  Created by labkumar.singh on 23/02/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import CustomTabBarBadge
+
+class FirstViewController: UIViewController {
+    
+    var count = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(updateBadge))
+        view.addGestureRecognizer(tap)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func updateBadge () {
+        count = count + 1
+        
+        tabBarItem.setTabBadgeValue("\(count)")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
